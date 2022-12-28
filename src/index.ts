5acdacb5fe444eb62acacd90e0d471bbf38bca17
MinhajSadik <MinhajoSadik@gmail.com>
2022-12-28 08:13:27 +0600

    [DONE] logger package for npm

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export class Log {
+  static success(msg: string) {
+    console.log(`%c ${msg}`, "color: green");
+  }
+  static error(msg: string) {
+    console.error(`%c ${msg}`, "color: red");
+  }
+
+  static info(msg: string) {
+    console.log(`%c ${msg}`, "color: black; background: yellow");
+  }
+
+  static debug(msg: string) {
+    console.log({ msg });
+  }
+}
